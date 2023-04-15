a0c5352090fd9cedac5b828d160ec54b499f58e3
John Lu <john493@michaels.com>
2023-04-15 10:42:27 +0800

    修改多线程调用方式

--- channel/http/http_channel.py
+++ channel/http/http_channel.py
@@ -1,12 +1,11 @@
 # encoding:utf-8
 import base64
 import json
-import logging
 import os
 from datetime import timedelta
-import time
-from flask_socketio import SocketIO, send, emit
-import eventlet
+
+import nest_asyncio
+from flask_socketio import SocketIO
 from flask import Flask, request, render_template, make_response
 from flask import jsonify
 from flask_cors import CORS
@@ -16,15 +15,16 @@ from larksuiteoapi.event import handle_event
 from larksuiteoapi.model import OapiRequest
 from channel.channel import Channel
 from channel.http import auth
-from common import const, functions, log
+from common import const, log
 from common.generator import generate_uuid
-from config import channel_conf, channel_conf_val
+from config import channel_conf
 from model.azure.azure_model import AZURE
 from channel.feishu.common_service import conf
 from service.file_training_service import upload_file_service
 from common.db.dbconfig import db
 import asyncio
 
+nest_asyncio.apply()
 http_app = Flask(__name__, template_folder='templates', static_folder='static', )
 # 自动重载模板文件
 http_app.jinja_env.auto_reload = True
@@ -167,13 +167,13 @@ async def return_stream(data):
     try:
         async for final, response in HttpChannel().handle_stream(data=data):
             if final:
-                logging.info("Final:" + response)
+                log.info("Final:" + response)
                 socketio.server.emit(
                     'final', {'content': response, 'messageID': data['messageID'], 'final': final}, request.sid,
                     namespace="/chat")
                 disconnect()
             else:
-                # logging.info("reply:" + response)
+                # log.info("reply:" + response)
                 socketio.sleep(0.01)
                 socketio.server.emit(
                     'reply', {'content': response, 'messageID': data['messageID'], 'final': final}, request.sid,
@@ -182,7 +182,7 @@ async def return_stream(data):
 
     except Exception as e:
         disconnect()
-        logging.warning("[http]emit:{}", e)
+        log.warning("[http]emit:{}", e)
 
 
 def run_async_test(data):
@@ -198,7 +198,7 @@ def stream(data):
         disconnect()
         return
     # data = json.loads(data)
-    logging.info("message:" + data['msg'])
+    log.info("message:" + data['msg'])
     if data:
         asyncio.run(return_stream(data))
 
@@ -208,13 +208,13 @@ def connect():
     if not auth.identify(request):
         disconnect()
         return
-    logging.info('connected')
+    log.info('connected')
     socketio.emit('connected', {'info': "connected"}, namespace='/chat')
 
 
 @socketio.on('disconnect', namespace='/chat')
 def disconnect():
-    logging.info('disconnect')
+    log.info('disconnect')
     socketio.server.disconnect(request.sid, namespace="/chat")
 
 
@@ -223,12 +223,12 @@ def disconnect():
 #     @socketio.on('message')
 #     def test_message(message):
 #         data = json.loads(message)
-#         logging.INFO("data"+data)
+#         log.INFO("data"+data)
 #         if not auth.identify(request):
-#             logging.INFO("Cookie error")
+#             log.INFO("Cookie error")
 #             return
 #         for completion in HttpChannel().handle_text(data=data, stream=True):
-#             logging.info('result:'.format(completion))
+#             log.info('result:'.format(completion))
 #             socketio.emit('response', {'content': completion})
 #
 #     @socketio.on('broadcast')
@@ -237,11 +237,11 @@ def disconnect():
 #
 #     @socketio.on('connect')
 #     def test_connect(arg):
-#         logging.info('Client connected')
+#         log.info('Client connected')
 #
 #     @socketio.on('disconnect')
 #     def test_disconnect():
-#         logging.info('Client dis connected')
+#         log.info('Client dis connected')
 #
 #     # @sio.on('my_event')
 #     # def my_event(data):
@@ -327,9 +327,9 @@ def webhook_event():
 
 # @socketio.on('message', namespace='/chat')
 # def handle_promote(data):
-#     logging.info("message:" + data)
+#     log.info("message:" + data)
 #     if not auth.identify(request):
-#         logging.INFO("Cookie error")
+#         log.INFO("Cookie error")
 #         return
 #     for completion in HttpChannel().handle_text(data=data):
 #         socketio.emit('response', {'content': completion})
@@ -337,4 +337,4 @@ def webhook_event():
 
 # @socketio.on('connect', namespace='/chat')
 # def handle_connect():
-#     logging.info('Client connected')
+#     log.info('Client connected')
--- model/openai/open_ai_model.py
+++ model/openai/open_ai_model.py
@@ -28,102 +28,13 @@ class OpenAIModel(Model):
 
             new_query = Session.build_session_query(query, from_user_id)
             log.debug("[OPEN_AI] session query={}".format(new_query))
-
-            if context.get('stream'):
-                # reply in stream
-                return self.reply_text_stream(query, new_query, from_user_id)
-
             reply_content = self.reply_text(new_query, from_user_id, 0)
             log.debug("[OPEN_AI] new_query={}, user={}, reply_cont={}".format(new_query, from_user_id, reply_content))
             if reply_content and query:
                 Session.save_session(query, reply_content, from_user_id)
             return reply_content
-
         elif context.get('type', None) == 'IMAGE_CREATE':
             return self.create_img(query, 0)
-
-    def reply_text(self, query, user_id, retry_count=0):
-        try:
-            response = openai.Completion.create(
-                model="text-davinci-003",  # 对话模型的名称
-                prompt=query,
-                temperature=0.9,  # 值在[0,1]之间，越大表示回复越具有不确定性
-                max_tokens=1200,  # 回复最大的字符数
-                top_p=1,
-                frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
-                presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
-                stop=["\n\n\n"]
-            )
-            res_content = response.choices[0]['text'].strip().replace('<|endoftext|>', '')
-            log.info("[OPEN_AI] reply={}".format(res_content))
-            return res_content
-        except openai.error.RateLimitError as e:
-            # rate limit exception
-            log.warn(e)
-            if retry_count < 1:
-                time.sleep(5)
-                log.warn("[OPEN_AI] RateLimit exceed, 第{}次重试".format(retry_count + 1))
-                return self.reply_text(query, user_id, retry_count + 1)
-            else:
-                return "提问太快啦，请休息一下再问我吧"
-        except Exception as e:
-            # unknown exception
-            log.exception(e)
-            Session.clear_session(user_id)
-            return "请再问我一次吧"
-
-    async def reply_text_stream(self, query, context, retry_count=0):
-        try:
-            user_id = context['from_user_id']
-            new_query = Session.build_session_query(query, user_id)
-            res = openai.ChatCompletion.create(
-                model=model_conf(const.OPEN_AI).get("model") or "gpt-3.5-turbo",  # 对话模型的名称
-                messages=new_query,
-                temperature=model_conf(const.OPEN_AI).get("temperature", 0.75),
-                # 熵值，在[0,1]之间，越大表示选取的候选词越随机，回复越具有不确定性，建议和top_p参数二选一使用，创意性任务越大越好，精确性任务越小越好
-                # max_tokens=4096,  # 回复最大的字符数，为输入和输出的总数
-                # top_p=model_conf(const.OPEN_AI).get("top_p", 0.7),,  #候选词列表。0.7 意味着只考虑前70%候选词的标记，建议和temperature参数二选一使用
-                frequency_penalty=model_conf(const.OPEN_AI).get("frequency_penalty", 0.0),
-                # [-2,2]之间，该值越大则越降低模型一行中的重复用词，更倾向于产生不同的内容
-                presence_penalty=model_conf(const.OPEN_AI).get("presence_penalty", 1.0),
-                # [-2,2]之间，该值越大则越不受输入限制，将鼓励模型生成输入中不存在的新词，更倾向于产生不同的内容
-                stream=True
-            )
-            full_response = ""
-            for chunk in res:
-                log.debug(chunk)
-                if (chunk["choices"][0]["finish_reason"] == "stop"):
-                    break
-                chunk_message = chunk['choices'][0]['delta'].get("content")
-                if (chunk_message):
-                    full_response += chunk_message
-                yield False, full_response
-            Session.save_session(query, full_response, user_id)
-            log.info("[chatgpt]: reply={}", full_response)
-            yield True, full_response
-
-        except openai.error.RateLimitError as e:
-            # rate limit exception
-            log.warn(e)
-            if retry_count < 1:
-                time.sleep(5)
-                log.warn("[CHATGPT] RateLimit exceed, 第{}次重试".format(retry_count + 1))
-                yield True, self.reply_text_stream(query, user_id, retry_count + 1)
-            else:
-                yield True, "提问太快啦，请休息一下再问我吧"
-        except openai.error.APIConnectionError as e:
-            log.warn(e)
-            log.warn("[CHATGPT] APIConnection failed")
-            yield True, "我连接不到网络，请稍后重试"
-        except openai.error.Timeout as e:
-            log.warn(e)
-            log.warn("[CHATGPT] Timeout")
-            yield True, "我没有收到消息，请稍后重试"
-        except Exception as e:
-            # unknown exception
-            log.exception(e)
-            Session.clear_session(user_id)
-            yield True, "请再问我一次吧"
 
     def _process_reply_stream(
             self,
