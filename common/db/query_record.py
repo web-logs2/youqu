3b5b8d0cf8af0d60665fea5642c5ac30e1f81e70
John Lu <john493@michaels.com>
2023-04-23 17:26:18 +0800

    support query records

---
+++ common/db/query_record.py
@@ -0,0 +1,29 @@
+
+from peewee import (
+    Model,
+    IntegerField,
+    CharField,
+    BooleanField,
+    DateTimeField,
+    AutoField
+)
+
+from common.db.dbconfig import db
+
+class QueryRecord(Model):
+    id = AutoField()
+    user_id = CharField(unique=False, max_length=64)
+    conversation_id = CharField(unique=False, max_length=64)
+    query = CharField(unique=False, max_length=30000)
+    reply = CharField(unique=False, max_length=30000)
+    created_time = DateTimeField()
+    updated_time = DateTimeField()
+
+    class Meta:
+        database = db
+        table_name = "query_reocrd"
+
+# 如果数据库中不存在表，则创建表
+db.connect()
+db.create_tables([QueryRecord], safe=True)
+db.close()
