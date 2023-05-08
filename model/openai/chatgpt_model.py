14af19a9bb4ca3e012c2bd9564a1bfde5c1d78b7
John Lu <john493@michaels.com>
2023-05-08 23:21:49 +0800

    support conversation record saving

--- model/openai/chatgpt_model.py
+++ model/openai/chatgpt_model.py
@@ -68,7 +68,7 @@ class ChatGPTModel(Model):
             response = openai.ChatCompletion.create(
                 model=model_conf(const.OPEN_AI).get("model") or "gpt-3.5-turbo",  # 对话模型的名称
                 messages=query,
-                temperature=0.9,  # 值在[0,1]之间，越大表示回复越具有不确定性
+                temperature=0.8,  # 值在[0,1]之间，越大表示回复越具有不确定性
                 top_p=1,
                 frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
