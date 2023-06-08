9759ecf48fadce47975c6cde539281d8e864655c
Robin Dai <510223272@qq.com>
2023-06-08 11:55:55 +0800

    build index by Babbage, query by GPT3.5 with embedding

--- common/menu_functions/public_train_methods.py
+++ common/menu_functions/public_train_methods.py
@@ -2,42 +2,103 @@ from langchain import OpenAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from llama_index import StorageContext, load_index_from_storage, ResponseSynthesizer, ServiceContext, LLMPredictor
-from llama_index.indices.document_summary import GPTDocumentSummaryIndex, DocumentSummaryIndexRetriever
+from llama_index.indices.document_summary import GPTDocumentSummaryIndex, DocumentSummaryIndexRetriever, \
+    DocumentSummaryIndexEmbeddingRetriever
 from llama_index.indices.response import ResponseMode
 from llama_index.query_engine import RetrieverQueryEngine
 
-from common.const import MODEL_GPT_35_TURBO, MODEL_TEXT_BABBAGE, MODEL_TEXT_BABBAGE_001
+from common.const import MODEL_GPT_35_TURBO, MODEL_TEXT_BABBAGE_001
+
+
+current_learning_model = MODEL_TEXT_BABBAGE_001
+current_querying_model = MODEL_GPT_35_TURBO
+
+
+# index_llm_predictor = LLMPredictor(llm=OpenAI(
+#     streaming=True,
+#     # use_async=True,
+#     temperature=0,
+#     model_name=current_learning_model,
+#     # callbacks=[StreamingStdOutCallbackHandler()]
+# ))
+# index_service_context = ServiceContext.from_defaults(
+#     llm_predictor=index_llm_predictor,
+#     # chunk_size_limit=1024
+# )
+# index_response_synthesizer = ResponseSynthesizer.from_args(
+#     streaming=True,
+#     # use_async=True,
+#     service_context=index_service_context
+# )
+
+
+# query_llm_predictor = LLMPredictor(llm=ChatOpenAI(
+#     streaming=True,
+#     # use_async=True,
+#     temperature=0,
+#     model_name=current_querying_model,
+#     # callbacks=[StreamingStdOutCallbackHandler()]
+# ))
+# query_service_context = ServiceContext.from_defaults(
+#     llm_predictor=query_llm_predictor,
+#     # chunk_size_limit=1024
+# )
+# query_response_synthesizer_chatgpt = ResponseSynthesizer.from_args(
+#     streaming=True,
+#     # use_async=True,
+#     service_context=query_service_context
+# )
 
 
 def public_train_documents(documents):
-    # return GPTVectorStoreIndex.from_documents(documents)
-
-    # llm_predictor_chatgpt = LLMPredictor(llm=OpenAI(
-    #     streaming=True,
-    #     # use_async=True,
-    #     temperature=0,
-    #     model_name=MODEL_TEXT_BABBAGE_001
-    # ))
-    llm_predictor_chatgpt = LLMPredictor(llm=ChatOpenAI(
-        streaming=True,
+    index_llm_predictor = LLMPredictor(llm=OpenAI(
+        # streaming=True,
         # use_async=True,
         temperature=0,
-        model_name=MODEL_GPT_35_TURBO,
+        model_name=current_learning_model,
         # callbacks=[StreamingStdOutCallbackHandler()]
     ))
-    service_context = ServiceContext.from_defaults(
-        llm_predictor=llm_predictor_chatgpt,
-        chunk_size_limit=2048
+    index_service_context = ServiceContext.from_defaults(
+        llm_predictor=index_llm_predictor,
+        # chunk_size_limit=1024
     )
-    response_synthesizer = ResponseSynthesizer.from_args(
-        response_mode=ResponseMode.TREE_SUMMARIZE,
+    index_response_synthesizer = ResponseSynthesizer.from_args(
+        # streaming=True,
         # use_async=True,
-        service_context=service_context
+        service_context=index_service_context
     )
+    # return GPTVectorStoreIndex.from_documents(documents)
     return GPTDocumentSummaryIndex.from_documents(
         documents=documents,
-        service_context=service_context,
-        response_synthesizer=response_synthesizer
+        service_context=index_service_context,
+        response_synthesizer=index_response_synthesizer
+    )
+
+
+def public_load_index(index_path):
+    # rebuild storage context
+    index_llm_predictor = LLMPredictor(llm=OpenAI(
+        # streaming=True,
+        # use_async=True,
+        temperature=0,
+        model_name=current_learning_model,
+        # callbacks=[StreamingStdOutCallbackHandler()]
+    ))
+    index_service_context = ServiceContext.from_defaults(
+        llm_predictor=index_llm_predictor,
+        # chunk_size_limit=1024
+    )
+    index_response_synthesizer = ResponseSynthesizer.from_args(
+        # streaming=True,
+        # use_async=True,
+        service_context=index_service_context
+    )
+    storage_context = StorageContext.from_defaults(persist_dir=index_path)
+    return load_index_from_storage(
+        storage_context=storage_context,
+        index_type=GPTDocumentSummaryIndex,
+        service_context=index_service_context,
+        response_synthesizer=index_response_synthesizer
     )
 
 
@@ -50,56 +111,38 @@ def public_query_documents(index_path, query_keywords):
     # )
 
     # Document Summary Index
-    llm_predictor_babbage = LLMPredictor(llm=OpenAI(
+    query_llm_predictor = LLMPredictor(llm=ChatOpenAI(
         streaming=True,
         # use_async=True,
         temperature=0,
-        model_name=MODEL_TEXT_BABBAGE_001
+        model_name=current_querying_model,
+        # callbacks=[StreamingStdOutCallbackHandler()]
     ))
-    llm_predictor_chatgpt = LLMPredictor(llm=ChatOpenAI(
-        streaming=True,
-        # use_async=True,
-        temperature=0,
-        model_name=MODEL_GPT_35_TURBO,
-        callbacks=[StreamingStdOutCallbackHandler()]
-    ))
-    service_context_babbage = ServiceContext.from_defaults(
-        llm_predictor=llm_predictor_babbage,
+    query_service_context = ServiceContext.from_defaults(
+        llm_predictor=query_llm_predictor,
         # chunk_size_limit=1024
     )
-    service_context_chatgpt = ServiceContext.from_defaults(
-        llm_predictor=llm_predictor_chatgpt,
-        # chunk_size_limit=1024
-    )
-    response_synthesizer_babbage = ResponseSynthesizer.from_args(
+    query_response_synthesizer = ResponseSynthesizer.from_args(
         streaming=True,
         # use_async=True,
-        service_context=service_context_babbage
+        service_context=query_service_context
     )
-    response_synthesizer_chatgpt = ResponseSynthesizer.from_args(
-        streaming=True,
-        # use_async=True,
-        service_context=service_context_chatgpt
-    )
-    # rebuild storage context
-    storage_context = StorageContext.from_defaults(persist_dir=index_path)
-    index = load_index_from_storage(
-        storage_context=storage_context,
-        index_type=GPTDocumentSummaryIndex,
-        service_context=service_context_babbage,
-        response_synthesizer=response_synthesizer_babbage
-    )
-    retriever = DocumentSummaryIndexRetriever(
+    index = public_load_index(index_path)
+    # query_retriever = DocumentSummaryIndexRetriever(
+    #     index=index,
+    #     service_context=query_service_context
+    # )
+    query_retriever = DocumentSummaryIndexEmbeddingRetriever(
         index=index,
-        service_context=service_context_chatgpt
+        service_context=query_service_context
     )
     query_engine = RetrieverQueryEngine.from_args(
         streaming=True,
         # use_async=True,
         # optimizer=SentenceEmbeddingOptimizer(threshold_cutoff=0.7),
-        retriever=retriever,
-        service_context=service_context_chatgpt,
-        response_synthesizer=response_synthesizer_chatgpt
+        top_k=1,
+        retriever=query_retriever,
+        service_context=query_service_context,
+        response_synthesizer=query_response_synthesizer
     )
-
     return query_engine.query(query_keywords)
