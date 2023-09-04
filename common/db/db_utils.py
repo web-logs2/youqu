53cd4710c236e2bcb541e9a697d5b719ee2b6bc0
Robin Dai <510223272@qq.com>
2023-09-04 15:53:26 +0800

    fix table name set as user, change to flexible

--- common/db/db_utils.py
+++ common/db/db_utils.py
@@ -7,7 +7,7 @@ def add_field_if_not_exist(table_name, field_name, field_properties):
     # create a field in existing table if not exist
     # TODO use the following sql to check if multiple fields exists in one table
     #  SHOW COLUMNS FROM user WHERE Field IN ('avatar', 'available', 'available_balance');
-    query = "SHOW COLUMNS FROM user LIKE '{}'".format(field_name)
+    query = "SHOW COLUMNS FROM {} LIKE '{}'".format(table_name, field_name)
     result = db.execute_sql(query)
     is_field_exist = False
     for row in result:
