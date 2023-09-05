62408a18dae31395491e3d9cd94f55f9d3715c41
Robin Dai <510223272@qq.com>
2023-09-05 17:59:12 +0800

    add http status code enum and customize error code

---
+++ common/error_code.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class ErrorCode(Enum):
+    # 1xx series, informational response
+
+    # 2xx series, successful
+    #   200, request succeed but business error
+    no_information_update = 20001
+
+    # 3xx series, redirection
+
+    # 4xx series, client error
+    #   401, unauthorized
+    no_user_found = 40101
+
+    # 5xx series, server error
+    database_operation_error = 50001
+
+class HTTPStatusCode(Enum):
+    ok = 200
+
+    bad_request = 400
+    unauthorized = 401
+    payment_required = 402
+    forbidden = 403
+    not_found = 404
+    request_timeout = 408
+
+    internal_server_error = 500
+    bad_gateway = 502
+    service_unavailable = 503
+    gateway_timeout = 504
